41462d8099cddbbbf9d9c7961bcd4a3acf4aafb4
Carlos Barboza <cadusb2@gmail.com>
2023-06-30 08:56:17 -0300

    Creating error status for FormGroup and Input components

--- src/components/FormGroup/styles.js
+++ src/components/FormGroup/styles.js
@@ -1,7 +1,15 @@
 import styled from 'styled-components';
 
 export const Container = styled.div`
-& + & {
-  margin-top: 16px;
-}
+  & + & {
+    margin-top: 16px;
+  }
+
+  small {
+    color: ${({ theme }) => theme.colors.danger.main};
+    font-size: 12px;
+    margin-top: 8px;
+    display: block;
+
+  }
 `
